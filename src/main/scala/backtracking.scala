4b520db4f8c40de2589f3b564a0d7868b2ce0869
P. William Hozier <phozier@umass.edu>
2016-05-12 22:10:48 -0400

    Reduction of grid to box using linear algebra concepts. Finalizing box search.

--- src/main/scala/backtracking.scala
+++ src/main/scala/backtracking.scala
@@ -1,10 +1,12 @@
+import scala.math._
+
 object backtracking extends algorithm{
 
   def solve(board: Seq[Seq[Int]]):Boolean = {
     if(end_of_grid(board)){
       return true
     }
-    backtrack(((0,0), board))
+    backtrack(((3,3), board))
     true
   }
 
@@ -22,15 +24,25 @@ object backtracking extends algorithm{
 
       // overview: check for collisions w new board.
       is_valid_grid((position, mutating_board), i)
-
-
     })
 
     true
-
-
   }
 
+
+  // overview: find starting position of smaller box within grid
+  def boxf(position:(Int, Int), board:Seq[Seq[Int]]):(Int, Int)={
+    val row = position._1
+    val column = position._2
+    (row - ((row) % sqrt(board.length).toInt),
+    column - ((column) % sqrt(board.length).toInt))
+  }
+
+
+  // overview: compute and return box using linear algebra
+  def get_box(x: Int, y: Int, board:Seq[Seq[Int]]) = {
+    board.slice(x, x+ sqrt(board.length).toInt).transpose.slice(y, y+sqrt(board.length).toInt).transpose
+  }
 
   // overview: checks whether the position's units are still valid
   // row, column, boxStartRow
@@ -43,10 +55,16 @@ object backtracking extends algorithm{
       !validate_primary(position._1, board, number)) return false
 
     // overview: check "box"
+    val (x, y):(Int, Int) = boxf(position, board)
+    get_box(x,y, board).foreach(_.foreach(
+      // iterate through cells of box
 
+    ))
     true
   }
 
+
+  // overview: done.
   def validate_primary(index:Int, board: Seq[Seq[Int]], number:Int):Boolean = {
     var count:Int = 0;
     board(index).foreach( (i:Int)=> (i == number) match {
@@ -66,6 +84,6 @@ object backtracking extends algorithm{
     board.foreach(_.foreach( // for each list/row
       (cell:Int) => if(cell == 0) return false
     ))
-    return true
+    true
   }
 }
