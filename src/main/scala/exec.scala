794c149433efe2513a4a82c68f2e27bc25a3fd6d
P. William Hozier <phozier@umass.edu>
2016-05-14 06:47:03 -0400

    created callback param for engine

--- src/main/scala/exec.scala
+++ src/main/scala/exec.scala
@@ -7,15 +7,15 @@ object exec{
   // overview: analysis callback
   private def analysis(i:algorithm, board:Seq[Seq[Int]]):Unit ={
     val start = System.nanoTime
+    val r = "\n\n\n\n\n"
 
     // run analysis iff the algorithm returns a validated solution
     if(i.solve(board)){
       val stop = System.nanoTime
-      printf("Algorithm:\n%s\nElapsed: \n[%fs]\n\n\n\n\n",
-        i.toString.split('$')(0), (stop - start)/1000000000.0)
-    } else printf("[No solution]\n\n\n\n\n")
+      printf("Algorithm:\n%s\nElapsed: \n[%fs]%s",
+        i.toString.split('$')(0), (stop - start)/1000000000.0, r)
+    } else printf("[No solution]%s", r)
   }
-
 
 
   def main(args: Array[String]): Unit = {
