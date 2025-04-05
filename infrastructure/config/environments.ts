bbb1b1d83bd79a50c27ac69e974cfb5cb040075d
P William Hozier <hozier@users.noreply.github.comm>
2025-04-05 00:06:54 -0400

    wip

--- infrastructure/config/environments.ts
+++ infrastructure/config/environments.ts
@@ -3,5 +3,4 @@ export const SERVICE_NAME = "play4s-service-prod";
 export const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
-  IMAGE_TAG_OR_DIGEST: process.env.IMAGE_TAG_OR_DIGEST || "latest", // Default to "latest" if not provided
 };
